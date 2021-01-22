45a3b2da9770d5a273a44a7c1b737bb17e88d0dd
huseyinyildiz <huseyinyildiz005@gmail.com>
2021-01-22 21:24:42 +0300

    .netcore crud

---
+++ ScaffoldCRUDDotNetCore/Models/TestDBContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ScaffoldCRUDDotNetCore.Models
+{
+    public partial class TestDBContext : DbContext
+    {
+        public TestDBContext()
+        {
+        }
+
+        public TestDBContext(DbContextOptions<TestDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<EmployeeMaster> EmployeeMasters { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+       //         optionsBuilder.UseSqlServer("Server=HUSEYINWEB\\SQLEXPRESS;Database=TestDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Turkish_CI_AS");
+
+            modelBuilder.Entity<EmployeeMaster>(entity =>
+            {
+                entity.HasKey(e => e.EmployeeId)
+                    .HasName("PK__Employee__7AD04FF166FF99A3");
+
+                entity.ToTable("EmployeeMaster");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.EmployeeFirstName)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EmployeeLastName)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
